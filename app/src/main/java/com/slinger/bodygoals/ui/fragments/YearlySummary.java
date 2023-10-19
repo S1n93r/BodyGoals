8da1033ac37f02413930ea4a30bd63d95b5ba763
Mirco <mirco00riemer@aol.com>
2023-10-19 22:23:36 +0200

    Added a view for a yearly summary but without any data connected yet

---
+++ app/src/main/java/com/slinger/bodygoals/ui/fragments/YearlySummary.java
@@ -0,0 +1,58 @@
+package com.slinger.bodygoals.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.slinger.bodygoals.R;
+import com.slinger.bodygoals.databinding.FragmentYearlySummaryBinding;
+import com.slinger.bodygoals.ui.ViewModel;
+
+public class YearlySummary extends Fragment {
+
+    private ViewModel viewModel;
+
+    private FragmentYearlySummaryBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getActivity() != null)
+            viewModel = new ViewModelProvider(getActivity()).get(ViewModel.class);
+
+        registerLiveDataObserver();
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        binding = FragmentYearlySummaryBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonToOverview.setOnClickListener(addGoalView -> NavHostFragment.findNavController(YearlySummary.this).navigate(R.id.action_yearly_summary_fragment_to_OverviewFragment));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void registerLiveDataObserver() {
+
+    }
+}

3ea35e0e9aca0eba926312671ec29a7a98413310
Mirco <mirco00riemer@aol.com>
2024-06-14 16:28:00 +0200

    Fixed issue with ExerciseListConverter

--- app/src/main/java/com/slinger/bodygoals/model/exercises/ExerciseListConverter.java
+++ app/src/main/java/com/slinger/bodygoals/model/exercises/ExerciseListConverter.java
@@ -4,28 +4,20 @@ import androidx.room.TypeConverter;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
-import com.slinger.bodygoals.model.Goal;
-import com.slinger.bodygoals.model.util.AdapterFactory;
 
 import java.lang.reflect.Type;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExerciseListConverter {
 
-    private static final TypeAdapter<LocalDate> localDateTypeAdapter = AdapterFactory.createLocalDateAdapter();
-
     @TypeConverter
-    public static List<Goal> fromString(String value) {
+    public static List<Exercise> fromString(String value) {
         Type type = new TypeToken<ArrayList<Exercise>>() {
         }.getType();
 
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDate.class, localDateTypeAdapter)
-                .create();
+        Gson gson = new GsonBuilder().create();
 
         return gson.fromJson(value, type);
     }
@@ -33,9 +25,7 @@ public class ExerciseListConverter {
     @TypeConverter
     public static String fromArrayList(List<Exercise> exercises) {
 
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDate.class, localDateTypeAdapter)
-                .create();
+        Gson gson = new GsonBuilder().create();
 
         return gson.toJson(exercises);
     }
